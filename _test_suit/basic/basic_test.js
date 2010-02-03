a414095ab547d42c43a94226d4f1775a26feaf57
kanashimi <kanashimi@meicho.com.tw>
2010-02-03 10:52:32 +0800

    Adding test suit

---
+++ _test_suit/basic/basic_test.js
@@ -0,0 +1,22 @@
+
+
+/*
+	Do full test for the poorest condition.
+	所有必要的基本（最小）功能皆已在前一個階段測試完成。
+*/
+
+
+//alert(document.getElementsByTagName);
+
+//	Test local variable defined by <code>var</code> ..
+
+
+//	test keyword
+
+//	test operator
+
+//	[test version]
+
+//	test DOM
+
+
