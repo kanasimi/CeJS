004bf5e89c27076eba7ad22a60d4266b51d1a7d2
kanasimi <kanasimi@gmail.com>
2018-09-09 12:38:37 +0800

    fix dir

--- application/net/work_crawler.js
+++ application/net/work_crawler.js
@@ -262,6 +262,7 @@ function module_code(library_namespace) {
 		main_directory : (library_namespace.platform.nodejs
 				&& process.mainModule ? process.mainModule.filename
 				.match(/[^\\\/]+$/)[0].replace(/\.js$/i, '') : process.cwd()
+				.replace(/[\\\/]+$/, '')
 				|| library_namespace.env('home') || '.')
 				// main_directory 必須以 path separator 作結。
 				+ path_separator,
