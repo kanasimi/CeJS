813cce461f47da0f0da0b44507a55eb14764fa4b
kanasimi <kanasimi@gmail.com>
2018-12-29 08:15:51 +0800

    fix bug / error / typo

--- application/net/work_crawler.js
+++ application/net/work_crawler.js
@@ -3335,7 +3335,7 @@ function module_code(library_namespace) {
 				image_data.done = true;
 			}
 			// 注意: 此時 image_data 可能是 undefined
-			_this.onerror('未指定圖像資料', image_data);
+			this.onerror('未指定圖像資料', image_data);
 			typeof callback === 'function' && callback('invalid_data');
 			return;
 		}
