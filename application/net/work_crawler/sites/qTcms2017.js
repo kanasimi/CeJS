6f14784448fb33bf44ee68a6a8aeec03d5901080
kanasimi <kanasimi@gmail.com>
2019-10-22 06:12:11 +0800

    對於非utf-8編碼之中文，不能使用 atob()

--- application/net/work_crawler/sites/qTcms2017.js
+++ application/net/work_crawler/sites/qTcms2017.js
@@ -286,8 +286,10 @@ function module_code(library_namespace) {
 
 			var chapter_data = html.between('qTcms_S_m_murl_e="', '"');
 			if (chapter_data) {
-				// 對於非utf-8編碼之中文，不能使用 atob()???
-				chapter_data = atob(chapter_data).split("$qingtiandy$");
+				// 對於非utf-8編碼之中文，不能使用 atob()
+				// e.g., http://www.aikanmh.cn/xuanhuan/zhutianji/499631.html
+				chapter_data = base64_decode(chapter_data)
+						.split("$qingtiandy$");
 			}
 			if (!Array.isArray(chapter_data)) {
 				library_namespace.warn({
@@ -295,6 +297,7 @@ function module_code(library_namespace) {
 				});
 				return;
 			}
+			// console.log(chapter_data);
 			// console.log(JSON.stringify(chapter_data));
 			// console.log(chapter_data.length);
 			// library_namespace.set_debug(6);
@@ -315,7 +318,9 @@ function module_code(library_namespace) {
 			// 設定必要的屬性。
 			chapter_data = {
 				image_list : chapter_data.map(function(url) {
-					url = encodeURI(url);
+					// 2019/10/20: 採用 base64_decode() 取代 atob() 後，
+					// aikanmh 不可再 encodeURI()。
+					// url = encodeURI(url);
 
 					// 获取当前图片 function f_qTcms_Pic_curUrl_realpic(v)
 					// http://www.xatxwh.com/template/skin1/css/d7s/js/show.20170501.js?20190117082944
@@ -343,7 +348,7 @@ function module_code(library_namespace) {
 								+ "&picid="
 								+ html.between('qTcms_S_m_id="', '"')
 								+ "&m_httpurl="
-								+ escape(atob(html.between(
+								+ escape(base64_decode(html.between(
 										'qTcms_S_m_mhttpurl="', '"')));
 						// Should get Status Code: 302 Found
 					}
@@ -359,6 +364,63 @@ function module_code(library_namespace) {
 		}
 
 	};
+
+	// --------------------------------------------------------------------------------------------
+
+	// http://www.aikanmh.cn/template/skin1/css/d7s/js/show.20170501.js?20191014154954
+	function utf8_decode(str_data) {
+		var tmp_arr = [], i = 0, ac = 0, c1 = 0, c2 = 0, c3 = 0;
+		str_data += '';
+		while (i < str_data.length) {
+			c1 = str_data.charCodeAt(i);
+			if (c1 < 128) {
+				tmp_arr[ac++] = String.fromCharCode(c1);
+				i++;
+			} else if ((c1 > 191) && (c1 < 224)) {
+				c2 = str_data.charCodeAt(i + 1);
+				tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6)
+						| (c2 & 63));
+				i += 2;
+			} else {
+				c2 = str_data.charCodeAt(i + 1);
+				c3 = str_data.charCodeAt(i + 2);
+				tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12)
+						| ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+		}
+		return tmp_arr.join('');
+	}
+
+	// 對於非utf-8編碼之中文，不能使用 atob()
+	function base64_decode(data) {
+		var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+		var o1, o2, o3, h1, h2, h3, h4, bits, i = 0, ac = 0, dec = "", tmp_arr = [];
+		if (!data) {
+			return data;
+		}
+		data += '';
+		do {
+			h1 = b64.indexOf(data.charAt(i++));
+			h2 = b64.indexOf(data.charAt(i++));
+			h3 = b64.indexOf(data.charAt(i++));
+			h4 = b64.indexOf(data.charAt(i++));
+			bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+			o1 = bits >> 16 & 0xff;
+			o2 = bits >> 8 & 0xff;
+			o3 = bits & 0xff;
+			if (h3 == 64) {
+				tmp_arr[ac++] = String.fromCharCode(o1);
+			} else if (h4 == 64) {
+				tmp_arr[ac++] = String.fromCharCode(o1, o2);
+			} else {
+				tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+			}
+		} while (i < data.length);
+		dec = tmp_arr.join('');
+		dec = utf8_decode(dec);
+		return dec;
+	}
 
 	// --------------------------------------------------------------------------------------------
 
