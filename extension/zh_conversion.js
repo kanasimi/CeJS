f359eba6f49dcc807511cab82187ad9bfe79f568
kanasimi <kanasimi@gmail.com>
2019-12-19 05:42:56 +0800

    + 篩選或增減 conversion files

--- extension/zh_conversion.js
+++ extension/zh_conversion.js
@@ -2,7 +2,7 @@
  * @name CeL function for 繁簡中文字詞彙轉換。
  * 
  * TODO:<br />
- * 在量大的時候，此方法速度頗慢。<br />
+ * 在量大的時候，此方法速度頗慢。 Using Map()<br />
  * words conversion
  * 
  * @fileoverview 本檔案包含了繁體/簡體中文轉換的 functions。
@@ -20,7 +20,7 @@
 	});
 
  </code>
- * @see https://github.com/BYVoid/OpenCC
+ * @see https://github.com/BYVoid/OpenCC https://zhconvert.org/
  * @since 2014/6/17 22:39:16
  */
 
@@ -64,7 +64,8 @@ function module_code(library_namespace) {
 
 	// ------------------------------------------------------------------------
 
-	var FLAG = 'gi',
+	// new RegExp(key, REPLACE_FLAG);
+	var REPLACE_FLAG = 'gi',
 	// using BYVoid / OpenCC 開放中文轉換 (Open Chinese Convert) table.
 	// https://github.com/BYVoid/OpenCC/tree/master/data/dictionary
 	dictionary_base = library_namespace.get_module_path(this.id, 'OpenCC'
@@ -79,7 +80,7 @@ function module_code(library_namespace) {
 		// 事前轉換表。
 		if (options && options.pre)
 			text = (new pair(options.pre, {
-				flag : options.flag || FLAG
+				flag : options.flag || REPLACE_FLAG
 			})).convert(text);
 
 		CN_to_TW.conversions.forEach(function(conversion) {
@@ -89,15 +90,19 @@ function module_code(library_namespace) {
 		// 事後轉換表。
 		if (options && options.post)
 			text = (new pair(options.post, {
-				flag : options.flag || FLAG
+				flag : options.flag || REPLACE_FLAG
 			})).convert(text);
 
 		return text;
 	}
 
-	CN_to_TW.files = ('STPhrases,STCharacters,TWPhrasesName,TWPhrasesIT'
+	CN_to_TW.files = 'STPhrases,STCharacters,TWPhrasesName,TWPhrasesIT'
 	// 因此得要一個個 replace。
-	+ ',TWPhrasesOther,TWVariants,TWVariantsRevPhrases').split(',');
+	+ ',TWPhrasesOther,TWVariants,TWVariantsRevPhrases';
+
+	// 若要篩選或增減 conversion files，可參考範例：
+	// start_downloading() @ CeL.application.net.work_crawler.task
+	CN_to_TW.files = CN_to_TW.files.split(',');
 
 	CN_to_TW.initialization = function() {
 		CN_to_TW.conversions = CN_to_TW.files.map(function(file_name, index) {
@@ -110,24 +115,47 @@ function module_code(library_namespace) {
 			});
 		});
 
+		// keys_to_remove
+		var remove_keys = Object.create(null);
 		// 手動修正表。
 		CN_to_TW.conversions.push(new pair(null, {
 			path : dictionary_base
 					.replace(/[^\\\/]+[\\\/]$/, 'corrections.txt'),
+			item_processor : function(item) {
+				var matched = item.match(/^-([^\t\n]{1,30})$/);
+				if (!matched) {
+					return item;
+				}
+				remove_keys[matched[1]] = true;
+				return '';
+			},
 			remove_comments : true
 		}));
 
+		if (!library_namespace.is_empty_object(remove_keys)) {
+			remove_keys = Object.keys(remove_keys);
+			CN_to_TW.conversions.forEach(function(conversion) {
+				if (false && conversion.pair.皮膚) {
+					console.log(conversion);
+					throw conversion.pair.皮膚;
+				}
+				conversion.remove(keys);
+			});
+		}
+
 		// 設定事前轉換表。
-		if (CN_to_TW.pre)
+		if (CN_to_TW.pre) {
 			CN_to_TW.conversions.unshift(new pair(CN_to_TW.pre, {
-				flag : CN_to_TW.flag || FLAG
+				flag : CN_to_TW.flag || REPLACE_FLAG
 			}));
+		}
 
 		// 設定事後轉換表。
-		if (CN_to_TW.post)
+		if (CN_to_TW.post) {
 			CN_to_TW.conversions.push(new pair(CN_to_TW.post, {
-				flag : CN_to_TW.flag || FLAG
+				flag : CN_to_TW.flag || REPLACE_FLAG
 			}));
+		}
 	};
 
 	CN_to_TW.file = function(from, to, target_encoding) {
