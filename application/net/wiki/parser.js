c331515dbcad73c7cade7175352e3bde597d66f4
kanasimi <kanasimi@gmail.com>
2020-01-14 20:15:01 +0800

    add/fix comments / debug logs

--- application/net/wiki/parser.js
+++ application/net/wiki/parser.js
@@ -1061,11 +1061,13 @@ function module_code(library_namespace) {
 	// @inner
 	function preprocess_section_link_token(token, options) {
 		if (typeof options.preprocess_section_link_token === 'function') {
-			token = options.preprocess_section_link_token(token);
+			token = options.preprocess_section_link_token(token, options);
 		}
 		// console.log(token);
 
 		if (token.type === 'transclusion' && wiki_API.template_functions) {
+			console.log(options);
+			console.log(wiki_API.template_functions);
 			var expand_template = wiki_API.template_functions[options.site_name
 					|| wiki_API.site_name(wiki_API.session_of_options(options))];
 			expand_template = expand_template
