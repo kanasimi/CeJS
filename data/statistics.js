68f2fb152f2443503e97dac8117a46e24c558dd5
kanasimi <kanasimi@gmail.com>
2020-12-26 07:13:28 +0800

    + function statistics(data_array)

--- data/statistics.js
+++ data/statistics.js
@@ -6,10 +6,12 @@
  * 
  * @since
  * @example <code>
- * CeL.run('data.statistics',function(){
- * 	// ..
- * });
- * </code>
+	CeL.run('data.statistics', function() {
+		var data_array = [1, 2, 3];
+		var statistics = CeL.statistics(data_array);
+		console.log(statistics);
+	});
+	</code>
  * 
  * @see <a href="http://www.jstat.org/" accessdate="2011/10/22 10:6">jStat : a
  *      JavaScript statistical library</a>
@@ -36,25 +38,48 @@ typeof CeL === 'function' && CeL.run({
 function module_code(library_namespace) {
 
 	/**
-	 * null module constructor
-	 * 
 	 * @class 處理統計的 functions
+	 * 
+	 * @see 集中趨勢 https://en.wikipedia.org/wiki/Central_tendency
 	 */
-	var _// JSDT:_module_
-	= function() {
-		// null module constructor
-	};
+	function statistics(data_array) {
+		// assert: data_array = [{Number},...]
+
+		var statistics_status = Object.create(null);
+		// summation
+		var sum = 0, count = 0, variance = 0;
+		for (var index = 0, length = data_array.length; index < length; index++) {
+			var data = data_array[index];
+			count++;
+			sum += data;
+
+			variance += data * data;
+
+			// TODO: 中位數、眾數
+			// https://stackoverflow.com/questions/4201292/on-algorithm-to-find-the-median-of-a-collection-of-numbers
+		}
+
+		Object.assign(statistics_status, {
+			count : count,
+			sum : sum,
+			// 算術平均數 arithmetic mean, average
+			mean : sum / count
+		});
+
+		// 變異數
+		statistics_status.variance = variance = variance / count
+				- statistics_status.mean * statistics_status.mean;
+		// 標準差 standard deviation
+		statistics_status.SD = Math.sqrt(variance);
+
+		return statistics_status;
+	}
 
 	/**
 	 * for JSDT: 有 prototype 才會將之當作 Class
 	 */
-	_// JSDT:_module_
+	statistics// JSDT:_module_
 	.prototype = {};
 
-	/**
-	 * ...
-	 */
-
-	return (_// JSDT:_module_
-	);
+	return statistics;
 }
