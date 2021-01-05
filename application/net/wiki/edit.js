9d1211f6f14d8e2f3d95c7f70103591fd58da384
kanasimi <kanasimi@gmail.com>
2021-01-06 06:16:19 +0800

    fix wiki_API_edit.denied

--- application/net/wiki/edit.js
+++ application/net/wiki/edit.js
@@ -645,12 +645,17 @@ function module_code(library_namespace) {
 		var page_data;
 		if (wiki_API.is_page_data(content)) {
 			page_data = content;
-			if (!(content = wiki_API.content_of(content)))
-				return;
+			content = wiki_API.content_of(content);
 		}
+		// assert: !content || typeof content === 'string'
 
-		content = content.replace(/<!--[\s\S]*-->/g, '').replace(
-				/<nowiki\s*>[\s\S]*<\/nowiki>/g, '');
+		if (typeof content === 'string') {
+			content = content.replace(/<!--[\s\S]*-->/g, '').replace(
+					/<nowiki\s*>[\s\S]*<\/nowiki>/g, '');
+		}
+		if (!content)
+			return;
+
 		library_namespace.debug('contents to test: [' + content + ']', 3,
 				'wiki_API_edit.denied');
 
