a7d3009f99c294e1db05cbf28a9870a3036ea5d5
kanasimi <kanasimi@gmail.com>
2021-06-11 19:34:08 +0800

    fix https://github.com/kanasimi/work_crawler/issues/514 : 去除圖檔的結尾

--- application/net/work_crawler/sites/webtoon.js
+++ application/net/work_crawler/sites/webtoon.js
@@ -181,8 +181,8 @@ function module_code(library_namespace) {
 			while (matched = PATTERN_image.exec(html)) {
 				chapter_data.image_list.push({
 					url : encodeURI(matched[1]
-					// 去掉?type=q70s的部分 畫質較好
-					.replace(/type=q\d{2}s?(?:&|$)/, ''))
+					// 去掉?type=q70s的部分 畫質較好 q70是手機版 q90是電腦版
+					.replace(/\?.*/, ''))
 				});
 			}
 
