f0a0ab28fe00d9c32ff05ca7b80a2dbc691d1da0
kanasimi <kanasimi@gmail.com>
2022-03-18 07:19:05 +0800

    fix 便签

--- application/net/wiki/template_functions/zhmoegirl.js
+++ application/net/wiki/template_functions/zhmoegirl.js
@@ -64,7 +64,7 @@ function module_code(library_namespace) {
 	// for get_all_anchors()
 	function expand_template_A(options) {
 		var parameters = this.parameters;
-		// {{a|显示文字|锚点名称}}
+		// {{a|锚点名称|显示文字}}
 		return '<span id="' + parameters[1] + '">'
 				+ (parameters[2] || parameters[1]) + '</span>';
 	}
